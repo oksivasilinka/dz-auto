4c94c77aa94fc6dee5989f5c3a828cea872c21e3
Oksana <oksana.s.kovalchuk@gmail.com>
2023-06-17 11:45:33 +0300

    dz8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,34 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.map(el => ({...el})).sort(function (a, b) {
+                if (action.payload === 'up') {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    return 0;
+                } else {
+                    if (b.name > a.name) {
+                        return 1;
+                    }
+                    if (b.name < a.name) {
+                        return -1;
+                    }
+                    return 0;
+                }
+
+            });
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
